d3aa568f2e0f7a0d6c10accd67c89cdd5da433ea
Gonzalo <gonzalo.saavedra.m@ug.uchile.cl>
2020-04-27 16:49:44 -0400

    Agregado nodo pausa
    Modificado mapa de entrada:
     -> Agregado "Pause": esc

---
+++ scripts/Pause.gd
@@ -0,0 +1,18 @@
+extends Control
+
+func _input(event):
+	if event.is_action_pressed('Pause'):
+		var new_pause_state = not get_tree().paused
+		get_tree().paused = new_pause_state
+		visible = new_pause_state
+		$CanvasLayer/VBoxContainer.visible = new_pause_state
+
+
+func _on_Resume_pressed():
+	visible = false
+	$CanvasLayer/VBoxContainer.visible = false
+	get_tree().paused = false
+
+
+func _on_Main_Menu_pressed():
+	get_tree().change_scene("res://scenes/menus/Main_menu.tscn")
