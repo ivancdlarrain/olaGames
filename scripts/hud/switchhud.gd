11442471211e491f627ffc9de7be21c8c1924511
ivanc <53668571+ivancdlarrain@users.noreply.github.com>
2020-05-03 16:31:38 -0400

    added HUD for color switch
    - hud represents pressing of keys

---
+++ scripts/hud/switchhud.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+onready var lkey = $left
+onready var dkey = $down
+onready var rkey = $right
+
+func _hold_key(key):
+	key.frame += 1
+	
+func _release_key(key):
+	key.frame -= 1
+
+func _physics_process(delta):
+	#Pressing key
+	if Input.is_action_just_pressed("ui_left"):
+		_hold_key(lkey)
+	if Input.is_action_just_pressed("ui_down"):
+		_hold_key(dkey)
+	if Input.is_action_just_pressed("ui_right"):
+		_hold_key(rkey)
+	
+	#Releasing key
+	if Input.is_action_just_released("ui_left"):
+		_release_key(lkey)
+	if Input.is_action_just_released("ui_down"):
+		_release_key(dkey)	
+	if Input.is_action_just_released("ui_right"):
+		_release_key(rkey)	
--- scripts/levels scripts/Tutorial2 scripts/WallJump Instruction.gd
+++ scripts/levels scripts/Tutorial2 scripts/WallJump Instruction.gd
@@ -10,7 +10,7 @@ func _ready():
 
 func _on_Area2D_body_entered(body):
 	if !done:
-		$Tween.interpolate_property(self, "modulate:a", 0, 255, 1.0, Tween.TRANS_SINE, Tween.EASE_IN)
+		$Tween.interpolate_property(self, "modulate:a", 0, 1, 1.0 , Tween.TRANS_SINE, Tween.EASE_IN)
 		$Tween.start()
 		done = true
 	
