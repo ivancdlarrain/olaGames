abb592438cfec42834dee24a9ec9794f378b5b48
ivanc <53668571+ivancdlarrain@users.noreply.github.com>
2020-05-03 01:50:33 -0400

    tutorial tests

--- scripts/textscripts/RichTextLabel.gd
+++ scripts/textscripts/RichTextLabel.gd
@@ -1,10 +1,9 @@
 extends RichTextLabel
 
 func _ready():
-	pass
+	modulate.a = 0
 	
 
 func _on_Area2D_body_entered(body):
 	print("entered")
-	$Tween.interpolate_property(self, "modulate:a", 0 , 255, 2.0, Tween.TRANS_SINE, Tween.EASE_IN)
-	print(modulate.a)
+	modulate.a = 255
