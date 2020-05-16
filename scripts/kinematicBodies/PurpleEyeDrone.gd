5bdb2f0500aef8ab1d9faad91bf98b1c9a7039d4
Gonzalo <gonzalo.saavedra.m@ug.uchile.cl>
2020-05-16 03:01:20 -0400

    PurpleEyeDrone update
    - Only missing way to activate movement and explosion

--- scripts/kinematicBodies/PurpleEyeDrone.gd
+++ scripts/kinematicBodies/PurpleEyeDrone.gd
@@ -1,12 +1,67 @@
 extends Drone
 
-const SPEED = 20
+#movement:
+const MAX_SPEED = 50
+const SPEED_INCREASE = 10
+const DEACCEL = 5
+#animation:
+var facing_right = false
+#detection:
+var enemy_in_range = false
+#doing stuff:
+var activated = false
+
 
 
 func _ready():
-	set_gravity(0)
-	set_deaccel(0)
 	set_color_layer(2)
 	set_exp_transform(2)
 
 
+# for movement:
+
+func apply_movement(direction):
+	velocity += SPEED_INCREASE * direction
+	if velocity.distance_to(Vector2(0, 0)):
+		velocity = velocity.normalized() * MAX_SPEED
+	velocity = move_and_slide(velocity)
+	# Animation:
+	if velocity.x > 0 and !facing_right:
+		scale.x *= -1
+		facing_right = true
+	elif velocity.x < 0 and facing_right:
+		scale.x *= -1
+		facing_right = false
+		
+
+
+func apply_deaccel():
+	var s = sign(velocity.x)
+	velocity -= velocity.normalized() * DEACCEL
+	if sign(velocity.x) != s:
+		velocity = Vector2(0, 0)
+	velocity = move_and_slide(velocity)
+
+
+func take_damage():
+	_die()
+
+
+func _on_ActivationArea_body_entered(body):
+	if body in get_tree().get_nodes_in_group("drone_target"):
+		enemy_in_range = true
+
+
+
+#FIXME: this function is never used, therefore, the drone will never move or explode. :c
+func _on_AnimationPlayer_animation_finished(anim_name):
+	print("IT'S BEING USED!")
+	print('anim_name')
+	match anim_name:
+		"activation":
+			activated = true
+		"deactivation":
+			activated = false
+		"explosion":
+			_die()
+	
--- scripts/state_machines/PurpleEyeDrone.gd
+++ scripts/state_machines/PurpleEyeDrone.gd
@@ -1,16 +1,53 @@
 extends DroneStateMachine
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	add_state('idle')
+	add_state('aggro')
+	add_state('explosion')
+	call_deferred('set_state', states.idle)
+	._ready()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _state_logic(delta):
+	if states.aggro == state and parent.activated:
+		parent.apply_movement(ray_direction)
+	elif states.idle == state:
+		parent.apply_deaccel()
+	._state_logic(delta)
+
+
+func _get_transition(_delta):
+	match state:
+		states.idle:
+			if parent.enemy_in_range:
+				return states.explosion
+			elif found:
+				return states.aggro
+		states.aggro:
+			if !found:
+				return states.idle
+			if parent.enemy_in_range:
+				return states.explosion
+	return null
+
+
+func _enter_state(new_state, _old_state):
+	match new_state:
+		states.idle:
+			parent.playback.travel('idle')
+		states.aggro:
+			parent.playback.travel('aggro')
+		states.explosion:
+			parent.playback.travel('explosion')
+
+
+func _exit_state(old_state, _new_state):
+#	match old_state:
+#		states.idle:
+#			pass
+#		states.aggro:
+#			pass
+#		states.explosion:
+#			pass
+	pass
