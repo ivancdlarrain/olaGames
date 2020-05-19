4d3c3339d02ce36c4bb325a70d193616594fe6ef
ivanc <53668571+ivancdlarrain@users.noreply.github.com>
2020-05-18 22:40:54 -0400

    more changes to save
    -modified save slot check

--- scripts/save.gd
+++ scripts/save.gd
@@ -1,16 +1,15 @@
 extends Node
 
-const SAVE_PATH = "res://data/savegames/savegame.json"
+var save_path = "res://data/savegames/savegame.json"
 
-var save_slots = []
+var SAVE_SLOTS = [save_path%1, save_path%2, save_path%3]
 
 
 func _ready():
 	pass
 
-func save_game():
-#	if save_slot_used():
-#		pass
+func save_game(slot: int):
+	
 	var save_dict = {}
 	var nodes_to_save = get_tree().get_nodes_in_group("saveable")
 	for node in nodes_to_save:
@@ -18,18 +17,18 @@ func save_game():
 		pass
 	
 	var savefile = File.new()
-	savefile.open(SAVE_PATH, File.READ)
+	savefile.open(SAVE_SLOTS[slot - 1], File.READ)
 	
 	savefile.store_line(to_json(save_dict))
 	savefile.close()
 
-func load_game():
+func load_game(slot: int):
 	print("Loading Game")
 	var savefile = File.new()
-	if not savefile.file_exists(SAVE_PATH):
+	if not savefile.file_exists(SAVE_SLOTS[slot - 1]):
 		return
 	
-	savefile.open(SAVE_PATH, File.READ)
+	savefile.open(SAVE_SLOTS[slot - 1], File.READ)
 	var save_data = {}
 	save_data = JSON.parse(savefile.get_as_text()).result
 	
@@ -45,9 +44,12 @@ func load_game():
 			
 	savefile.close()
 	
-func save_slot_used(savefile: File):
-	if savefile.file_exists(SAVE_PATH):
+func save_slot_used(slot: int):
+	var savefile = File.new()
+	savefile.open(SAVE_SLOTS[slot - 1], File.READ)
+	if savefile.file_exists(SAVE_SLOTS[slot - 1]):
+		savefile.close()
 		return true
-	
+	return false
 	
 
