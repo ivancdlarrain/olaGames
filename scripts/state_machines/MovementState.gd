980bce7f227936d9bc41164fce4f60e9af9383df
Gonzalo <gonzalo.saavedra.m@ug.uchile.cl>
2020-05-22 13:03:34 -0400

    refactor to _input(event) in MovementState
    Now it checks for color before doing anything.

--- scripts/state_machines/MovementState.gd
+++ scripts/state_machines/MovementState.gd
@@ -9,7 +9,7 @@ func _ready():
 	add_state('jump')
 	add_state('fall')
 	add_state('dash')
-#	add_state('glide')
+	add_state('glide')
 	add_state('wall_slide')
 	add_state('pre_fall')
 	call_deferred('set_state', states.idle)
@@ -17,9 +17,9 @@ func _ready():
 
 func _state_logic(delta):
 	parent._handle_horizontal_move_input()
-	#Apply a cap to the fall speed if the player is wall-sliding
-#	if [states.wall_slide, states.glide].has(state):
-	if states.wall_slide ==state:
+	if [states.idle, states.run, states.pre_fall].has(state) and !parent.j_timer.is_stopped():
+			parent.velocity.y = -parent.jump_speed
+	if [states.wall_slide, states.glide].has(state):
 		parent._cap_gravity(delta)
 	else:
 		parent._apply_gravity(delta)
@@ -28,43 +28,53 @@ func _state_logic(delta):
 	parent._tile_detection()
 
 
-func _input(event):
-	if event.is_action_pressed('WASD_up'):
-		if [states.wall_slide].has(state):
-			parent._wall_jump()
-			parent.double_jump = true
-		elif color.state == color.states.orange:
-#			if [states.jump, states.fall, states.glide].has(state) and (parent.double_jump or parent.dj_cd.is_stopped()):
-			if [states.jump, states.fall].has(state) and (parent.double_jump or parent.dj_cd.is_stopped()):
-				parent.velocity.y = -parent.jump_speed
-				parent.double_jump = false
-				parent.dj_cd.start()
-			else: 
-				parent.j_timer.start()
-		else:
-			parent.j_timer.start()
-		
-	elif event.is_action_released("WASD_up"):
-		if [states.jump].has(state):
+func common_jump():
+	if states.wall_slide == state:
+		parent._wall_jump()
+		parent.double_jump = true
+	else:
+		parent.j_timer.start()
+
+
+func common_input_rest(event):
+	if event.is_action_released('WASD_up'):
+		if states.jump == state:
 			parent.velocity.y = parent.velocity.y * 0.5
-	
-	if [states.idle, states.run, states.pre_fall].has(state) and !parent.j_timer.is_stopped():
-			parent.velocity.y = -parent.jump_speed
-	
+
+
+func _input(event):
 	match color.state:
 		color.states.blue:
 			if event.is_action_pressed("special"):
 				if [states.run, states.fall, states.jump].has(state) and parent.dash_cd.is_stopped():
 					parent.dashing  = true
+			elif event.is_action_pressed('WASD_up'):
+				common_jump()
+			else:
+				common_input_rest(event)
+		
 		color.states.orange:
-			pass
+			if event.is_action_pressed('WASD_up'):
+				if states.wall_slide == state:
+					parent._wall_jump()
+					parent.double_jump = true
+				elif [states.jump, states.fall].has(state) and (parent.double_jump or parent.dj_cd.is_stopped()):
+					parent.velocity.y = -parent.jump_speed
+					parent.double_jump = false
+					parent.dj_cd.start()
+				else:
+					parent.j_timer.start()
+			else:
+				common_input_rest(event)
+		
 		color.states.purple:
 			if event.is_action_pressed("special"):
-#				if [states.jump, states.fall].has(state):
-#					parent.gliding = true
-				pass
-			elif event.is_action_released("special") or parent.on_floor or parent.is_on_wall():
-				parent.gliding = false	
+				if [states.jump, states.fall].has(state):
+					parent.gliding = true
+			elif event.is_action_pressed('WASD_up'):
+				common_jump()
+			if event.is_action_released("special") or parent.on_floor or parent.is_on_wall():
+				parent.gliding = false
 
 
 func _get_transition(_delta):
@@ -102,15 +112,15 @@ func _get_transition(_delta):
 			elif parent.dashing:
 				return states.dash
 			
-#			elif parent.gliding:
-#				return states.glide
+			elif parent.gliding:
+				return states.glide
 				
 			else:
 				if parent.velocity.y > 0: 
 					return states.fall
 				
-#				if parent.grav == parent.glide_grav:
-#					return states.glide
+				if parent.grav == parent.glide_grav:
+					return states.glide
 		states.fall:
 			if on_floor:
 				parent.double_jump = true
@@ -123,8 +133,8 @@ func _get_transition(_delta):
 				return states.wall_slide
 			elif parent.dashing:
 				return states.dash
-#			elif parent.gliding:
-#				return states.glide
+			elif parent.gliding:
+				return states.glide
 			else:
 				if parent.velocity.y < 0: 
 					return states.jump
@@ -137,17 +147,17 @@ func _get_transition(_delta):
 				else:
 					return states.run
 		
-#		states.glide:
-#			if !on_floor:
-#				if !parent.gliding:
-#					return states.fall
-#			if on_wall:
-#				return states.wall_slide
-#			if on_floor:
-#				if parent.velocity.x == 0:
-#					return states.idle
-#				else:
-#					return states.run
+		states.glide:
+			if !on_floor:
+				if !parent.gliding:
+					return states.fall
+			if on_wall:
+				return states.wall_slide
+			if on_floor:
+				if parent.velocity.x == 0:
+					return states.idle
+				else:
+					return states.run
 		
 		states.wall_slide:
 			if on_floor:
@@ -186,12 +196,12 @@ func _enter_state(new_state, _old_state):
 			parent.grav = 0
 			var x = parent.max_speed * 3.3 if parent.facing_right else parent.max_speed * -3.3
 			parent.velocity = parent.move_and_slide(Vector2(x, 0))
-#		states.glide:
-#			emit_signal("use_ground_collision", false)
-#			parent.playback.travel("fall")
-#			parent.grav = parent.glide_grav	
-#			if parent.velocity.y < 0:
-#				parent.velocity.y = 0
+		states.glide:
+			emit_signal("use_ground_collision", false)
+			parent.playback.travel("fall")
+			parent.grav = parent.glide_grav	
+			if parent.velocity.y < 0:
+				parent.velocity.y = 0
 		states.wall_slide:
 			emit_signal("use_ground_collision", false)
 			parent.playback.travel("fall")
@@ -212,5 +222,5 @@ func _exit_state(old_state, new_state):
 			parent.dash_cd.start()
 			if new_state == states.wall_slide:
 				parent.velocity = parent.move_and_slide(Vector2(0, -parent.max_speed))
-#		states.glide:
-#			parent.grav = parent.default_grav
+		states.glide:
+			parent.grav = parent.default_grav
