cd595e4f7889d71873fc160f0a6ca7096dad0123
ivanc <53668571+ivancdlarrain@users.noreply.github.com>
2020-05-28 18:51:58 -0400

    changed projectile
    - now kills the player
    - TO DO: rotate projectile sprite

--- assets/entities/projectile/BlueProjectile.gd
+++ assets/entities/projectile/BlueProjectile.gd
@@ -1,17 +1,15 @@
 extends Area2D
 
 
-const PROJECTILE_SPEED = 200
+const PROJECTILE_SPEED = 500
 var direction = Vector2()
 var velocity = Vector2()
-
-func _on_Area2D_body_entered(body):
-	if body in get_tree().get_nodes_in_group("drone_target"):
-		body._die()
-		queue_free()
+export var impact: PackedScene
+	
 		
 func _fire(direction: Vector2):
 	self.direction = direction
+	$AnimationPlayer.play("travel")
 
 func _physics_process(delta):
 	velocity = direction * PROJECTILE_SPEED * delta
@@ -19,3 +17,13 @@ func _physics_process(delta):
 	
 #	if position.x or position.y >= 1000:
 #		queue_free()
+
+
+func _on_Projectile_body_entered(body):
+	if body in get_tree().get_nodes_in_group("drone_target"):
+		body._die()
+	set_physics_process(false)
+	$AnimationPlayer.play("hit")
+
+func _remove():
+	queue_free()
